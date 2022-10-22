df69635ec257e43bcdeb2ff14fb8291e27417f8d
mahathu <m.hoffmann@systemli.org>
2022-10-22 17:25:10 +0200

    changed web interface framework to flask

--- server.py
+++ server.py
@@ -1,16 +1,18 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from flask import Flask
 
-app = FastAPI()
+app = Flask(__name__)
 
-
-@app.get("/")
-def read_root():
+@app.route("/")
+def main():
     with open("nohup.out", "r") as f:
         lines = f.readlines()
 
-        event_lines = [l for l in lines if "Sleeping" not in l]
+    event_lines = [l for l in lines if "Sleeping" not in l]
+    return f"""
+        <h2>last 5 updates</h2>
+        <textarea rows=5 style='width:100%;'>{''.join(lines)}</textarea>
 
-        html_content = f"<h2>last 5 updates</h2>{'<br>'.join(lines[-5:-1])}<hr><h2>last 5 events</h2>{'<br>'.join(event_lines[-5:-1])}"
-
-        return HTMLResponse(content=html_content, status_code=200)
+        <hr>
+        <h2>last 5 events</h2>
+        <textarea rows=5 style='width:100%;'>{''.join(event_lines)}</textarea>
+        """
