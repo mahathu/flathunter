4181be18d050bc776afb927456d38e6066e03904
mahathu <m.hoffmann@systemli.org>
2023-08-01 18:20:37 +0200

    updated seen ads and server code

--- server/server.py
+++ server/server.py
@@ -13,12 +13,12 @@ def run():
 @app.route("/")
 def main():
     try:
-        with open("logfile.log", "r") as f:
+        with open("../logfile.log", "r") as f:
             log_lines = [l.strip() for l in f.readlines()[-5:]]
     except FileNotFoundError:
         log_lines = []
 
-    df = pd.read_csv("seen_ads.csv").drop(["id", "zip_code"], axis=1).head(100)
+    df = pd.read_csv("../seen_ads.csv").drop(["id", "zip_code"], axis=1).head(100)
 
     table_html = df.to_html(
         header="true", index=False, table_id="dataframe", na_rep="/", classes=["stripe"]
